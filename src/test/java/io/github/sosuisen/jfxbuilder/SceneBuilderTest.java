9e3c4204894f357426f2234afc264de353adcbc7
sosuisen <hidekazu.kubota@gmail.com>
2025-08-06 16:26:01 +0900

    feat: add test

---
+++ src/test/java/io/github/sosuisen/jfxbuilder/SceneBuilderTest.java
@@ -0,0 +1,369 @@
+package io.github.sosuisen.jfxbuilder;
+
+import javafx.event.EventHandler;
+import javafx.geometry.NodeOrientation;
+import javafx.scene.Camera;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SceneBuilderTest {
+
+    private Parent rootNode;
+
+    @BeforeEach
+    void setUp() {
+        try {
+            javafx.application.Platform.startup(() -> {
+            });
+        } catch (IllegalStateException e) {
+            // Platform already started
+        }
+
+        rootNode = new VBox();
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with root parameter using withRoot")
+    void shouldCreateSceneBuilderWithRoot() {
+        SceneBuilder builder = SceneBuilder.withRoot(rootNode);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with root parameter using create")
+    void shouldCreateSceneBuilderWithRootUsingCreate() {
+        SceneBuilder builder = SceneBuilder.create(rootNode);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with root, width and height")
+    void shouldCreateSceneBuilderWithDimensions() {
+        double expectedWidth = 800.0;
+        double expectedHeight = 600.0;
+
+        SceneBuilder builder = SceneBuilder.create(rootNode, expectedWidth, expectedHeight);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+        assertEquals(expectedWidth, scene.getWidth(), 0.001);
+        assertEquals(expectedHeight, scene.getHeight(), 0.001);
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with root and paint fill")
+    void shouldCreateSceneBuilderWithFill() {
+        Paint expectedFill = Color.BLUE;
+
+        SceneBuilder builder = SceneBuilder.create(rootNode, expectedFill);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+        assertEquals(expectedFill, scene.getFill());
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with depth buffer enabled")
+    void shouldCreateSceneBuilderWithDepthBuffer() {
+        double expectedWidth = 800.0;
+        double expectedHeight = 600.0;
+        boolean depthBuffer = true;
+
+        SceneBuilder builder = SceneBuilder.create(rootNode, expectedWidth, expectedHeight, depthBuffer);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+    }
+
+    @Test
+    @DisplayName("Should create SceneBuilder with antialiasing")
+    void shouldCreateSceneBuilderWithAntialiasing() {
+        double expectedWidth = 800.0;
+        double expectedHeight = 600.0;
+        boolean depthBuffer = false;
+        SceneAntialiasing antialiasing = SceneAntialiasing.BALANCED;
+
+        SceneBuilder builder = SceneBuilder.create(rootNode, expectedWidth, expectedHeight, depthBuffer, antialiasing);
+        assertNotNull(builder);
+
+        Scene scene = builder.build();
+        assertNotNull(scene);
+        assertEquals(rootNode, scene.getRoot());
+    }
+
+    @Test
+    @DisplayName("Should set root using builder method")
+    void shouldSetRootUsingBuilderMethod() {
+        Parent newRoot = new Button("Test Button");
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .root(newRoot)
+                .build();
+
+        assertEquals(newRoot, scene.getRoot());
+    }
+
+    @Test
+    @DisplayName("Should set fill using builder method")
+    void shouldSetFillUsingBuilderMethod() {
+        Paint expectedFill = Color.GREEN;
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .fill(expectedFill)
+                .build();
+
+        assertEquals(expectedFill, scene.getFill());
+    }
+
+    @Test
+    @DisplayName("Should set user agent stylesheet")
+    void shouldSetUserAgentStylesheet() {
+        String expectedStylesheet = "test-stylesheet.css";
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .userAgentStylesheet(expectedStylesheet)
+                .build();
+
+        assertEquals(expectedStylesheet, scene.getUserAgentStylesheet());
+    }
+
+    @Test
+    @DisplayName("Should set cursor")
+    void shouldSetCursor() {
+        Cursor expectedCursor = Cursor.CROSSHAIR;
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .cursor(expectedCursor)
+                .build();
+
+        assertEquals(expectedCursor, scene.getCursor());
+    }
+
+    @Test
+    @DisplayName("Should set node orientation")
+    void shouldSetNodeOrientation() {
+        NodeOrientation expectedOrientation = NodeOrientation.RIGHT_TO_LEFT;
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .nodeOrientation(expectedOrientation)
+                .build();
+
+        assertEquals(expectedOrientation, scene.getNodeOrientation());
+    }
+
+    @Test
+    @DisplayName("Should set user data")
+    void shouldSetUserData() {
+        String expectedUserData = "test-data";
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .userData(expectedUserData)
+                .build();
+
+        assertEquals(expectedUserData, scene.getUserData());
+    }
+
+    @Test
+    @DisplayName("Should set key pressed event handler")
+    void shouldSetKeyPressedEventHandler() {
+        boolean[] keyPressed = { false };
+        EventHandler<? super KeyEvent> handler = e -> keyPressed[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onKeyPressed(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnKeyPressed());
+    }
+
+    @Test
+    @DisplayName("Should set key released event handler")
+    void shouldSetKeyReleasedEventHandler() {
+        boolean[] keyReleased = { false };
+        EventHandler<? super KeyEvent> handler = e -> keyReleased[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onKeyReleased(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnKeyReleased());
+    }
+
+    @Test
+    @DisplayName("Should set key typed event handler")
+    void shouldSetKeyTypedEventHandler() {
+        boolean[] keyTyped = { false };
+        EventHandler<? super KeyEvent> handler = e -> keyTyped[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onKeyTyped(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnKeyTyped());
+    }
+
+    @Test
+    @DisplayName("Should set mouse clicked event handler")
+    void shouldSetMouseClickedEventHandler() {
+        boolean[] mouseClicked = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseClicked[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseClicked(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseClicked());
+    }
+
+    @Test
+    @DisplayName("Should set mouse pressed event handler")
+    void shouldSetMousePressedEventHandler() {
+        boolean[] mousePressed = { false };
+        EventHandler<? super MouseEvent> handler = e -> mousePressed[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMousePressed(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMousePressed());
+    }
+
+    @Test
+    @DisplayName("Should set mouse released event handler")
+    void shouldSetMouseReleasedEventHandler() {
+        boolean[] mouseReleased = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseReleased[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseReleased(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseReleased());
+    }
+
+    @Test
+    @DisplayName("Should set mouse entered event handler")
+    void shouldSetMouseEnteredEventHandler() {
+        boolean[] mouseEntered = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseEntered[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseEntered(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseEntered());
+    }
+
+    @Test
+    @DisplayName("Should set mouse exited event handler")
+    void shouldSetMouseExitedEventHandler() {
+        boolean[] mouseExited = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseExited[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseExited(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseExited());
+    }
+
+    @Test
+    @DisplayName("Should set mouse moved event handler")
+    void shouldSetMouseMovedEventHandler() {
+        boolean[] mouseMoved = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseMoved[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseMoved(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseMoved());
+    }
+
+    @Test
+    @DisplayName("Should set mouse dragged event handler")
+    void shouldSetMouseDraggedEventHandler() {
+        boolean[] mouseDragged = { false };
+        EventHandler<? super MouseEvent> handler = e -> mouseDragged[0] = true;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .onMouseDragged(handler)
+                .build();
+
+        assertEquals(handler, scene.getOnMouseDragged());
+    }
+
+    @Test
+    @DisplayName("Should chain multiple builder methods")
+    void shouldChainMultipleBuilderMethods() {
+        Paint expectedFill = Color.YELLOW;
+        Cursor expectedCursor = Cursor.WAIT;
+        String expectedUserData = "chained-scene";
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .fill(expectedFill)
+                .cursor(expectedCursor)
+                .userData(expectedUserData)
+                .nodeOrientation(NodeOrientation.LEFT_TO_RIGHT)
+                .build();
+
+        assertEquals(rootNode, scene.getRoot());
+        assertEquals(expectedFill, scene.getFill());
+        assertEquals(expectedCursor, scene.getCursor());
+        assertEquals(expectedUserData, scene.getUserData());
+        assertEquals(NodeOrientation.LEFT_TO_RIGHT, scene.getNodeOrientation());
+    }
+
+    @Test
+    @DisplayName("Should apply custom consumer function")
+    void shouldApplyCustomConsumerFunction() {
+        Paint customFill = Color.PURPLE;
+
+        Scene scene = SceneBuilder.withRoot(rootNode)
+                .apply(sc -> sc.setFill(customFill))
+                .build();
+
+        assertEquals(customFill, scene.getFill());
+    }
+
+    @Test
+    @DisplayName("Should handle null parameters gracefully")
+    void shouldHandleNullParametersGracefully() {
+        assertDoesNotThrow(() -> {
+            Scene scene = SceneBuilder.withRoot(rootNode)
+                    .userAgentStylesheet(null)
+                    .userData(null)
+                    .build();
+            assertNotNull(scene);
+        });
+    }
+
+    @Test
+    @DisplayName("Should throw exception when root is null")
+    void shouldThrowExceptionWhenRootIsNull() {
+        assertThrows(RuntimeException.class, () -> {
+            SceneBuilder.withRoot(null).build();
+        });
+    }
+}
