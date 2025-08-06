ab073aa1889b7b1372c1036d0f02b0f34aa747e9
sosuisen <hidekazu.kubota@gmail.com>
2025-08-06 16:32:45 +0900

    feat: add children method

--- src/main/java/io/github/sosuisen/Main.java
+++ src/main/java/io/github/sosuisen/Main.java
@@ -4,18 +4,20 @@ import java.io.IOException;
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 
 public class Main {
 
-  public static void main(String[] args) {
-    try {
-      BuilderClassGenerator generator = new BuilderClassGenerator();
-      generator.generate(Button.class);
-      generator.generate(Scene.class);
-    } catch (IOException e) {
-      System.err.println("Error generating builder class: " + e.getMessage());
-      e.printStackTrace();
+    public static void main(String[] args) {
+        try {
+            BuilderClassGenerator generator = new BuilderClassGenerator();
+            generator.generate(Button.class);
+            generator.generate(Scene.class);
+            generator.generate(VBox.class);
+        } catch (IOException e) {
+            System.err.println("Error generating builder class: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
-  }
 
 }
