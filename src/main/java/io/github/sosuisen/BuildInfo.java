d3758935d4c6bf0b98c8eaba602cdb04f1302f0c
sosuisen <hidekazu.kubota@gmail.com>
2025-08-07 23:27:03 +0900

    feat: can change JavaFX version by profile

---
+++ src/main/java/io/github/sosuisen/BuildInfo.java
@@ -0,0 +1,21 @@
+package io.github.sosuisen;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class BuildInfo {
+    private static final Properties BUILD_PROPS = new Properties();
+
+    static {
+        try (InputStream is = BuildInfo.class.getResourceAsStream("/build.properties")) {
+            BUILD_PROPS.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read build.properties", e);
+        }
+    }
+
+    public static String getJavaFXVersion() {
+        return BUILD_PROPS.getProperty("javafx.version");
+    }
+}
--- src/main/java/io/github/sosuisen/Main.java
+++ src/main/java/io/github/sosuisen/Main.java
@@ -13,6 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         try {
+            var javaFxVersion = BuildInfo.getJavaFXVersion();
+            System.out.println("JavaFX Version: " + javaFxVersion);
+
             BuilderClassGenerator generator = new BuilderClassGenerator();
             generator.generate(Button.class);
             generator.generate(Scene.class);
