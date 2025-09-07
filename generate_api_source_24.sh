687606852e54980381f14eec55a56c229f2340ad
sosuisen <hidekazu.kubota@gmail.com>
2025-09-07 19:12:45 +0900

    fix: update dir structure

--- generate_api_source_24.sh
+++ generate_api_source_24.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mvn clean compile -pl generator
+mvn clean compile -pl generator -P javafx24,controls
 mvn javafx:run -pl generator -P javafx24,controls
 mvn javafx:run -pl generator -P javafx24,graphics
 mvn javafx:run -pl generator -P javafx24,media
